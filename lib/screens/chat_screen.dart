98b04f7df670363f031556d8f392052502409af4
fercansen <fercansen@gmail.com>
2021-01-13 02:36:03 +0300

    Addin hardcoded values to firebase to test

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -28,7 +28,11 @@ class ChatScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          Firestore.instance
+              .collection("chats/eLhUvpC6cwUctDqAZMRV/messages")
+              .add({"text": "This text was added by clicking the button"});
+        },
       ),
     );
   }
