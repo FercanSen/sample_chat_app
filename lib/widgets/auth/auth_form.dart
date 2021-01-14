4f7264bb468cf2f4c6b208d7e05031c665b2b072
fercansen <fercansen@gmail.com>
2021-01-15 01:00:02 +0300

    Setting up form validation

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -6,6 +6,24 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  var _userEmail = "";
+  var _userName = "";
+  var _userPassword = "";
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,24 +33,53 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains("@")) {
+                        return "Please enter a valid email address.";
+                        // TODO: Add proper error check.
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(labelText: "Email Address"),
+                    onSaved: (newValue) {
+                      _userEmail = newValue;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 3) {
+                        return "Username must be at least 3 characters long.";
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(labelText: "Username"),
+                    onSaved: (newValue) {
+                      _userName = newValue;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 5) {
+                        return "Password must be at least 5 characters long.";
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(labelText: "Password"),
                     obscureText: true,
+                    onSaved: (newValue) {
+                      _userPassword = newValue;
+                    },
                   ),
                   SizedBox(height: 12),
                   RaisedButton(
                     child: Text("Login"),
-                    onPressed: () {},
+                    onPressed: _trySubmit,
                   ),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
