a7faa0afb07a07bfe0b76e9574ff75bf8e790576
fercansen <fercansen@gmail.com>
2021-01-25 00:35:52 +0300

    Add profile picture button created

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -52,6 +52,13 @@ class _AuthFormState extends State<AuthForm> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  CircleAvatar(radius: 40),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.image),
+                    label: Text("Add Profile Picture"),
+                    textColor: Theme.of(context).primaryColor,
+                  ),
                   TextFormField(
                     key: ValueKey("email"),
                     validator: (value) {
